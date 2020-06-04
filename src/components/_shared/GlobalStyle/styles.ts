adeb28b165833718d153ba8d76f5752a412f2fd5
wadaryohei <ryohei.w0317wr@icloud.com>
2020-06-05 08:23:23 +0900

    GlobalStyleを調整

--- src/components/_shared/GlobalStyle/styles.ts
+++ src/components/_shared/GlobalStyle/styles.ts
@@ -1,8 +1,13 @@
+/**
+ * Style層
+ * - CSSを記述する
+ * - styledの引数でコンポーネントを受け取る
+ * - CSSのネストが深くなったらコンポーネント分割を検討する
+ */
 import { createGlobalStyle } from 'styled-components'
 import { Colors } from '../../../const/Colors'
 
 export const GlobalStyle = createGlobalStyle`
-
   *, *::before, *::after {
     margin: 0;
     padding: 0;
@@ -11,5 +16,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     font-family: Roboto, 'Helvetica Neue', Arial, 'Hiragino Kaku Gothic ProN', 'Hiragino Sans', Meiryo, sans-serif;
     color: ${Colors.black};
+  }
+
+  li {
+    list-style: none;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
   }
 `
