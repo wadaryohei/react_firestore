47e365e743f6fb82a6a521ca827857daf39ba330
wadaryohei <ryohei.w0317wr@icloud.com>
2020-08-05 20:53:28 +0900

    コメントを削除

--- src/components/_shared/ButtonList/styles.ts
+++ src/components/_shared/ButtonList/styles.ts
@@ -1,9 +1,3 @@
-/**
- * Style層
- * - CSSを記述する
- * - styledの引数でコンポーネントを受け取る
- * - CSSのネストが深くなったらコンポーネント分割を検討する
- */
 import styled from 'styled-components'
 import { Margin } from '../../../const/Margin'
 
